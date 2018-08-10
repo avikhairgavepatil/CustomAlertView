c5271f604227fae0af7ae1ce5b03607bcd2ee358
varun daga <varundaga@varuns-imac.local>
2018-08-10 12:34:24 +0530

    Initial Commit

---
+++ CustomAlertView/CustomAlertView/CustomAlertView.swift
@@ -0,0 +1,109 @@
+//
+//  CustomAlertView.swift
+//  CustomAlertView
+//
+//  Created by Daniel Luque Quintana on 16/3/17.
+//  Copyright © 2017 dluque. All rights reserved.
+//
+
+import UIKit
+
+class CustomAlertView: UIViewController {
+    
+    @IBOutlet weak var bottomHeight: NSLayoutConstraint!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var alertTextField: UITextField!
+    @IBOutlet weak var alertView: UIView!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var okButton: UIButton!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    var delegate: CustomAlertViewDelegate?
+    var selectedOption = "First"
+    let alertViewGrayColor = UIColor(red: 224.0/255.0, green: 224.0/255.0, blue: 224.0/255.0, alpha: 1)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: Notification.Name.UIKeyboardWillHide, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: Notification.Name.UIKeyboardWillChangeFrame, object: nil)
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(CustomAlertView.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+        alertTextField.becomeFirstResponder()
+    }
+    func adjustForKeyboard(notification: Notification) {
+        let userInfo = notification.userInfo!
+        
+        let keyboardScreenEndFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+        
+        if notification.name == Notification.Name.UIKeyboardWillHide {
+            //alertView.isHidden = true
+           self.dismiss(animated: true, completion: nil)
+        } else {
+            bottomHeight.constant = keyboardViewEndFrame.height-45
+        }
+    }
+    
+    func dismissKeyboard(){
+        delegate?.cancelButtonTapped()
+        //self.dismiss(animated: true, completion: nil)
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setupView()
+        animateView()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        view.layoutIfNeeded()
+        cancelButton.addBorder(side: .Top, color: alertViewGrayColor, width: 1)
+        cancelButton.addBorder(side: .Right, color: alertViewGrayColor, width: 1)
+        okButton.addBorder(side: .Top, color: alertViewGrayColor, width: 1)
+    }
+    
+    func setupView() {
+        alertView.layer.cornerRadius = 15
+        self.view.backgroundColor = UIColor.black.withAlphaComponent(0.4)
+    }
+    
+    func animateView() {
+        alertView.alpha = 0;
+        self.alertView.frame.origin.y = self.alertView.frame.origin.y + 50
+        UIView.animate(withDuration: 0.4, animations: { () -> Void in
+            self.alertView.alpha = 1.0;
+            self.alertView.frame.origin.y = self.alertView.frame.origin.y - 50
+        })
+    }
+    
+    @IBAction func onTapCancelButton(_ sender: Any) {
+        alertTextField.resignFirstResponder()
+        delegate?.cancelButtonTapped()
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func onTapOkButton(_ sender: Any) {
+        alertTextField.resignFirstResponder()
+        delegate?.okButtonTapped(selectedOption: selectedOption, textFieldValue: alertTextField.text!)
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func onTapSegmentedControl(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            print("First option")
+            selectedOption = "First"
+            break
+        case 1:
+            print("Second option")
+            selectedOption = "Second"
+            break
+        default:
+            break
+        }
+    }
+}
